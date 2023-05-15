5f4425c3e7e4118cc2441ee4c0059125ab40f466
chanhocode <chjo1245@gmail.com>
2023-05-15 15:06:35 +0900

    Feat: Server & 마신 물의양 업데이트 기능 구현 참조: #3

--- server/models/water.js
+++ server/models/water.js
@@ -1,20 +1,20 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports = class Water extends Sequelize.Model {
   static initiate(sequelize) {
     Water.init(
       {
         amount_of_water: {
-          type: Sequelize.STRING(10),
+          type: Sequelize.INTEGER,
           allowNull: false,
         },
       },
       {
         sequelize,
-        modelName: 'Water',
-        tableName: 'waters',
-        charset: 'utf8',
-        collate: 'utf8_general_ci',
+        modelName: "Water",
+        tableName: "waters",
+        charset: "utf8",
+        collate: "utf8_general_ci",
       }
     );
   }
--- server/routes/water.js
+++ server/routes/water.js
@@ -5,15 +5,16 @@ const router = express.Router();
 
 router.get("/weight/tumbler", async (req, res, next) => {
   try {
+    // ++++++++++++
     // 텀블러 무게 재는 코드 위치
-    console.log("start");
     const wait = (timeToDelay) =>
       new Promise((resolve) => setTimeout(resolve, timeToDelay));
     await wait(3000);
     let result = {
       weight: 800,
     };
-    console.log("result: ", result);
+    // ++++++++++++
+
     res.status(200).json(result);
   } catch (err) {
     console.dir(err);
@@ -22,25 +23,49 @@ router.get("/weight/tumbler", async (req, res, next) => {
 });
 
 //물 측정량 조회
-router.get("/check", async (req, res, next) => {
+router.patch("/amount", async (req, res, next) => {
   try {
-    const water = await Water.findAll({
-      type: req.body.type,
+    // 오늘 날짜
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = ("0" + (today.getMonth() + 1)).slice(-2);
+    let day = ("0" + today.getDate()).slice(-2);
+    let dateString = year + "-" + month + "-" + day;
+    // 오늘 날짜의 마신 물의 양 불러오기 없으면 생성
+    console.log("오늘 날짜: ", dateString);
+    const tumbler = await Water.findOrCreate({
+      where: { createdAt: `${dateString}%` },
+      defaults: {
+        amount_of_water: 0,
+      },
     });
-    res.status(201).json(water);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-//물 측정량 등록 post
-router.post("/create", async (req, res, next) => {
-  try {
-    const water = await Water.create({
-      type: req.body.type,
+    console.log(tumbler[0].dataValues.amount_of_water);
+    let currentAmount = tumbler[0].dataValues.amount_of_water;
+    // 전부 마심 버튼 클릭 시
+    if (req.body.drank_all) {
+      //amount_of_water + tumbler 최대 양
+      currentAmount = currentAmount + 800;
+    } else {
+      // amount_of_water + 텀블러 무게 측정-텀블러 무게
+      const up = 300;
+      // +++++++ 텀블러 무게 측정 코드 ++++++++
+      currentAmount = currentAmount + up;
+    }
+    console.log("물의 양: ", currentAmount);
+    console.log("::", tumbler[0].dataValues.createdAt);
+    // // 물의 양 업데이트 및 리턴
+    await Water.update(
+      {
+        amount_of_water: currentAmount,
+      },
+      {
+        where: { createdAt: tumbler[0].dataValues.createdAt },
+      }
+    );
+    res.status(201).json({
+      amount_of_water: currentAmount,
     });
-    res.status(201).json(water);
+    res.status(201).send("ok");
   } catch (err) {
     console.error(err);
     next(err);
