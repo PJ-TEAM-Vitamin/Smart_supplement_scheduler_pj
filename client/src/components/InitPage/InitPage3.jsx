8852592e012f23e740d6880f987b7fc3a3b82ea7
chanhocode <chjo1245@gmail.com>
2023-05-15 22:03:48 +0900

    Fix: 마신 물의 양 업데이트 기능 변경

--- client/src/components/InitPage/InitPage3.jsx
+++ client/src/components/InitPage/InitPage3.jsx
@@ -1,44 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Alarm, InitCommonHeader, InitPageContainer, MoveButton, SelectTimeForm, TimeInputItem } from './styles';
 import Clock from '../../utils/img/clock.png';
 const InitPage3 = ({ handleChangeState }) => {
+  const [isCheck1, setCheck1] = useState(false);
+  const [isCheck2, setCheck2] = useState(false);
 
-   const [isCheck1, setCheck1] = useState(false);
-   const [isCheck2, setCheck2] = useState(false);
+  const toggleBtn1 = () => {
+    setCheck1(isCheck1 => !isCheck1); // on,off 개념 boolean
+  };
 
-   const toggleBtn1 = () => {
-      setCheck1(isCheck1 => !isCheck1); // on,off 개념 boolean
-   };
+  const toggleBtn2 = () => {
+    setCheck2(isCheck2 => !isCheck2); // on,off 개념 boolean
+  };
+  // const arr = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  // const [pick, setPick] = useState(arr);
+  // const [select, setSelect] = useState([]);
 
-   const toggleBtn2 = () => {
-      setCheck2(isCheck2 => !isCheck2); // on,off 개념 boolean
-   };
-   // const arr = [{ id: 1 }, { id: 2 }, { id: 3 }];
-   // const [pick, setPick] = useState(arr);
-   // const [select, setSelect] = useState([]);
-
-
-   return (
-
+  return (
     <InitPageContainer>
       <Alarm>
         <img className="clock" src={Clock} alt="clock" />
         <InitCommonHeader className="title" style={{ margin: '0px' }}>
           알림 시간을 설정하세요
         </InitCommonHeader>
-         <div>
-            {"두번째 알람 추가"}
-            <button onClick={()=>toggleBtn1()}>
-               {isCheck1 ? "-" : "+"}
-            </button>
-            <div>
-               {"세번째 알람 추가"}
-               <button onClick={()=>toggleBtn2()}>
-                  {isCheck2 ? "-" : "+"}
-               </button>
-            </div>
-         </div>
+        <div>
+          {'두번째 알람 추가'}
+          <button onClick={() => toggleBtn1()}>{isCheck1 ? '-' : '+'}</button>
+          <div>
+            {'세번째 알람 추가'}
+            <button onClick={() => toggleBtn2()}>{isCheck2 ? '-' : '+'}</button>
+          </div>
+        </div>
 
         <SelectTimeForm>
           <div className="timeInput">
@@ -46,18 +39,18 @@ const InitPage3 = ({ handleChangeState }) => {
               <div className="timeLabel">첫번째 알람</div>
               <input type="time" name="time1" onChange={handleChangeState} />
             </TimeInputItem>
-             {isCheck1 && (
-                <TimeInputItem>
-                   <div className="timeLabel">두번째 알람</div>
-                   <input type="time" name="time2" onChange={handleChangeState} />
-                </TimeInputItem>
-             )}
-             {isCheck2 &&(
-                <TimeInputItem>
-                   <div className="timeLabel">세번째 알람</div>
-                   <input type="time" name="time3" onChange={handleChangeState} />
-                </TimeInputItem>
-             )}
+            {isCheck1 && (
+              <TimeInputItem>
+                <div className="timeLabel">두번째 알람</div>
+                <input type="time" name="time2" onChange={handleChangeState} />
+              </TimeInputItem>
+            )}
+            {isCheck2 && (
+              <TimeInputItem>
+                <div className="timeLabel">세번째 알람</div>
+                <input type="time" name="time3" onChange={handleChangeState} />
+              </TimeInputItem>
+            )}
           </div>
         </SelectTimeForm>
         <MoveButton>
--- server/routes/water.js
+++ server/routes/water.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const Water = require("../models/water");
+const User = require("../models/user");
 
 const router = express.Router();
 
 router.get("/weight/tumbler", async (req, res, next) => {
   try {
     // ++++++++++++
-    // 텀블러 무게 재는 코드 위치
+    // tumbler 무게 측정 python code 실행 _ return: 700
+    // const weight = await (read python code)
     const wait = (timeToDelay) =>
       new Promise((resolve) => setTimeout(resolve, timeToDelay));
     await wait(3000);
+    // ++++++++++++
     let result = {
       weight: 800,
     };
-    // ++++++++++++
 
     res.status(200).json(result);
   } catch (err) {
@@ -25,7 +27,7 @@ router.get("/weight/tumbler", async (req, res, next) => {
 //물 측정량 조회
 router.patch("/amount", async (req, res, next) => {
   try {
-    // 오늘 날짜
+    // 오늘 날짜 생성
     let today = new Date();
     let year = today.getFullYear();
     let month = ("0" + (today.getMonth() + 1)).slice(-2);
@@ -37,36 +39,53 @@ router.patch("/amount", async (req, res, next) => {
       where: { createdAt: `${dateString}%` },
       defaults: {
         amount_of_water: 0,
+        tumbler_count: 1,
       },
     });
-    console.log(tumbler[0].dataValues.amount_of_water);
-    let currentAmount = tumbler[0].dataValues.amount_of_water;
 
-    // 전부 마심 버튼 클릭 시
-    if (req.body.drank_all) {
-      //amount_of_water + tumbler 최대 양
-      currentAmount = currentAmount + 800;
+    const tumbler_Info = await User.findOne({
+      where: {
+        id: req.body.id,
+      },
+    });
+
+    console.log(tumbler_Info.dataValues);
+
+    const tumbler_weight = tumbler_Info.dataValues.tumbler_capacity;
+    const tumbler_capacity = tumbler_Info.dataValues.tumbler_capacity;
+    const DB_amount = tumbler[0].dataValues.amount_of_water;
+    const DB_count = tumbler[0].dataValues.tumbler_count;
+    // 최대 잔량 계산
+    const max_residual = tumbler_capacity * DB_count - DB_amount;
+    console.log("최대 잔량 계산: ", max_residual);
+    // ++++++++++++++++++
+    // tumbler 무게 측정 python code 실행 _ return: 700
+    const currentWeight = 700 - tumbler_weight;
+    // ++++++++++++++++++
+
+    let currentAmount;
+    let currentCount = DB_count;
+    if (currentWeight > max_residual) {
+      currentAmount = DB_amount + max_residual + currentWeight;
+      currentCount++;
     } else {
-      // amount_of_water + 텀블러 무게 측정-텀블러 무게
-      const up = 300;
-      // +++++++ 텀블러 무게 측정 코드 ++++++++
-      currentAmount = currentAmount + up;
+      currentAmount = DB_amount + currentWeight;
     }
-    console.log("물의 양: ", currentAmount);
-    console.log("::", tumbler[0].dataValues.createdAt);
-    // // 물의 양 업데이트 및 리턴
+    console.log(`총량: ${currentAmount}, count: ${currentCount}`);
+
+    // 물의 양 업데이트 및 리턴
     await Water.update(
       {
         amount_of_water: currentAmount,
+        tumbler_count: currentCount,
       },
       {
-        where: { createdAt: tumbler[0].dataValues.createdAt },
+        where: { id: tumbler[0].dataValues.id },
       }
     );
     res.status(201).json({
       amount_of_water: currentAmount,
     });
-    res.status(201).send("ok");
   } catch (err) {
     console.error(err);
     next(err);
